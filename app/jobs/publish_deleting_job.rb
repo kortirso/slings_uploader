6702bc44ea2f3716740101addc8293978a10ca07
kortirso <kortirso@gmail.com>
2017-04-14 15:54:19 +0300

    add deleting service for market

--- app/jobs/publish_deleting_job.rb
+++ app/jobs/publish_deleting_job.rb
@@ -3,5 +3,8 @@ class PublishDeletingJob < ApplicationJob
 
     def perform(params)
         PublishDeletingService.new({user: params[:user], publish: params[:publish]}).deleting
+        if params[:publish].market_item_id.present?
+            MarketPublishDeletingService.new({user: params[:user], publish: params[:publish]}).deleting
+        end
     end
 end
