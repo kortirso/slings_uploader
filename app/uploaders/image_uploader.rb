0104905efae549671cd7b45e06f49f8558f05d07
kortirso <kortirso@gmail.com>
2017-04-28 07:16:33 +0300

    update image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,6 +7,8 @@ class ImageUploader < CarrierWave::Uploader::Base
         "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
+    process resize_to_fit: [1000, 0]
+
     version :for_catalog do
         process resize_to_fill: [140,200]
     end
