72d2f766bc583c3363691d0d2441a8ab5e354fae
kortirso <kortirso@gmail.com>
2017-05-10 09:28:31 +0300

    fix

--- app/services/site_publish_creating_service.rb
+++ app/services/site_publish_creating_service.rb
@@ -13,7 +13,7 @@ class SitePublishCreatingService
         begin
             uri = URI(generate_uri)
             req = Net::HTTP::Post.new(uri)
-            req.set_form_data(name: publish.name, caption: publish.caption, price: publish.price, category_name: Album.find_by(album_id: album_id).album_name, image: "#{ENV['APP_HOST']}#{publish.product_image}")
+            req.set_form_data(name: publish.name, caption: publish.caption, price: publish.price, category_name: user.albums.find_by(album_id: album_id).album_name, image: "#{ENV['APP_HOST']}#{publish.product_image}")
             res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
             answer = JSON.parse(res.body)
             publish.update(site_item_id: answer['product']['id']) if answer['product'].present?
@@ -26,7 +26,7 @@ class SitePublishCreatingService
         begin
             uri = URI(generate_uri(true))
             req = Net::HTTP::Put.new(uri)
-            req.set_form_data(name: publish.name, caption: publish.caption, price: publish.price, category_name: Album.find_by(album_id: album_id).album_name, site_item_id: publish.site_item_id, image: "#{ENV['APP_HOST']}#{publish.product_image}")
+            req.set_form_data(name: publish.name, caption: publish.caption, price: publish.price, category_name: user.albums.find_by(album_id: album_id).album_name, site_item_id: publish.site_item_id, image: "#{ENV['APP_HOST']}#{publish.product_image}")
             Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
         rescue
             false
