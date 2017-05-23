b250c50be8b5733a4823b2be35a9e00945f6f485
kortirso <kortirso@gmail.com>
2017-05-23 10:58:58 +0300

    remove category from uploading to market

--- app/jobs/catalog_marketing_job.rb
+++ app/jobs/catalog_marketing_job.rb
@@ -2,7 +2,7 @@ class CatalogMarketingJob < ApplicationJob
     queue_as :default
 
     def perform(params)
-        params[:user].publishes.where(market_item_id: nil).each do |publish|
+        params[:user].publishes.includes(:product).not_published_in_market.where('products.category_id != ?', Category.find_by(name: 'Ткани').id).references(:products).each do |publish|
             MarketPublishCreatingService.new({user: params[:user], publish: publish, auto: true}).publishing
         end
     end
