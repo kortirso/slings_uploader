75ad10fde614cf6f18d7383e8bf885cf784f48d2
kortirso <kortirso@gmail.com>
2017-12-29 21:12:48 +0300

    fix migrations

--- db/migrate/20170415034303_rebuild_tables_for_attachments_fix.rb
+++
@@ -1,7 +0,0 @@
-class RebuildTablesForAttachmentsFix < ActiveRecord::Migration[5.0]
-    def change
-        #remove_column :publishes, :image
-        #remove_column :publishes, :photo_id
-        #add_column :attachments, :photo_id, :string
-    end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -15,128 +15,128 @@ ActiveRecord::Schema.define(version: 20170507164020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "albums", force: :cascade do |t|
-    t.integer  "album_id"
-    t.integer  "vk_group_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "album_name",  default: ""
-    t.index ["vk_group_id"], name: "index_albums_on_vk_group_id", using: :btree
-  end
-
-  create_table "archives", force: :cascade do |t|
-    t.integer  "vk_group_id"
-    t.integer  "album_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["vk_group_id"], name: "index_archives_on_vk_group_id", using: :btree
-  end
-
-  create_table "attachments", force: :cascade do |t|
-    t.string   "image"
-    t.integer  "product_id"
+  create_table "albums", id: :serial, force: :cascade do |t|
+    t.integer "album_id"
+    t.integer "vk_group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "photo_id"
-    t.integer  "parent_id"
-    t.integer  "publish_id"
-    t.index ["product_id"], name: "index_attachments_on_product_id", using: :btree
-    t.index ["publish_id"], name: "index_attachments_on_publish_id", using: :btree
+    t.string "album_name", default: ""
+    t.index ["vk_group_id"], name: "index_albums_on_vk_group_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name",        default: "", null: false
-    t.string   "slug"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "single_name", default: ""
-    t.index ["slug"], name: "index_categories_on_slug", unique: true, using: :btree
+  create_table "archives", id: :serial, force: :cascade do |t|
+    t.integer "vk_group_id"
+    t.integer "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["vk_group_id"], name: "index_archives_on_vk_group_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+  create_table "attachments", id: :serial, force: :cascade do |t|
+    t.string "image"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "photo_id"
+    t.integer "parent_id"
+    t.integer "publish_id"
+    t.index ["product_id"], name: "index_attachments_on_product_id"
+    t.index ["publish_id"], name: "index_attachments_on_publish_id"
+  end
+
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "single_name", default: ""
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
+  end
+
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "identities", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
+  create_table "identities", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "provider"
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_identities_on_user_id", using: :btree
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name",        default: "",    null: false
-    t.text     "caption"
-    t.integer  "price",       default: 0,     null: false
-    t.string   "slug"
-    t.integer  "category_id"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.boolean  "deleted",     default: false
-    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
-    t.index ["slug"], name: "index_products_on_slug", unique: true, using: :btree
+  create_table "products", id: :serial, force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.text "caption"
+    t.integer "price", default: 0, null: false
+    t.string "slug"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "deleted", default: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["slug"], name: "index_products_on_slug", unique: true
   end
 
-  create_table "publishes", force: :cascade do |t|
-    t.text     "caption"
-    t.integer  "price"
-    t.integer  "user_id"
-    t.integer  "product_id"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "name",           default: "",    null: false
-    t.integer  "album_id"
-    t.integer  "market_item_id"
-    t.boolean  "published",      default: false
-    t.integer  "site_item_id"
-    t.index ["album_id"], name: "index_publishes_on_album_id", using: :btree
-    t.index ["product_id"], name: "index_publishes_on_product_id", using: :btree
-    t.index ["user_id"], name: "index_publishes_on_user_id", using: :btree
+  create_table "publishes", id: :serial, force: :cascade do |t|
+    t.text "caption"
+    t.integer "price"
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
+    t.integer "album_id"
+    t.integer "market_item_id"
+    t.boolean "published", default: false
+    t.integer "site_item_id"
+    t.index ["album_id"], name: "index_publishes_on_album_id"
+    t.index ["product_id"], name: "index_publishes_on_product_id"
+    t.index ["user_id"], name: "index_publishes_on_user_id"
   end
 
-  create_table "sites", force: :cascade do |t|
-    t.integer  "user_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.json     "settings",   default: {}
-    t.index ["user_id"], name: "index_sites_on_user_id", using: :btree
+  create_table "sites", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.json "settings", default: {}
+    t.index ["user_id"], name: "index_sites_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",     null: false
-    t.string   "encrypted_password",     default: "",     null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,      null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.string   "token",                  default: ""
-    t.string   "role",                   default: "user", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "vk_groups", force: :cascade do |t|
-    t.integer  "user_id"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "group_id"
-    t.index ["user_id"], name: "index_vk_groups_on_user_id", using: :btree
+    t.string "token", default: ""
+    t.string "role", default: "user", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vk_groups", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "group_id"
+    t.index ["user_id"], name: "index_vk_groups_on_user_id"
   end
 
 end
