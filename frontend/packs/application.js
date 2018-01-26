ea1fb9b51dfab193bde08e4250b1e0ccec393e8c
kortirso <kortirso@gmail.com>
2018-01-26 22:04:14 +0300

    add instructions component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -9,6 +9,7 @@ import "components/metrics/metrics";
 
 import "components/users/users";
 import "components/product_form/product_form";
+import "components/instructions/instructions";
 
 import Rails from "rails-ujs";
 
