0e6d6ad37b6da1fc01194dde8e6304966b677d8e
kortirso <kortirso@gmail.com>
2018-01-26 21:16:31 +0300

    add components to main page

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,7 +1,13 @@
 import "init";
-import "image/image";
+import "images/image";
 
 import "components/welcome/welcome";
+import "components/page/page";
+
+import "components/navigation/navigation";
+import "components/metrics/metrics";
+
+import "components/users/users";
 
 import Rails from "rails-ujs";
 
