242875b57a1d8103151d27727896060a816231b1
kortirso <kortirso@gmail.com>
2018-01-26 22:42:19 +0300

    add categories component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -10,6 +10,8 @@ import "components/metrics/metrics";
 import "components/users/users";
 import "components/product_form/product_form";
 import "components/instructions/instructions";
+import "components/categories/categories";
+import "components/categories_navigation/categories_navigation";
 
 import Rails from "rails-ujs";
 
