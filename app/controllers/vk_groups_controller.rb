8b5678577f802b6eb1cca0c6f6afe4251aa65d2b
kortirso <kortirso@gmail.com>
2018-05-06 13:14:15 +0700

    add vk_groups controller specs

--- app/controllers/vk_groups_controller.rb
+++ app/controllers/vk_groups_controller.rb
@@ -1,5 +1,5 @@
 class VkGroupsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :update
+  skip_before_action :verify_authenticity_token
   before_action :find_vk_group
 
   def update
@@ -8,13 +8,12 @@ class VkGroupsController < ApplicationController
   end
 
   private def find_vk_group
-    @vk_group = VkGroup.find_by(id: params[:id])
-    render_not_found if @vk_group.nil? || @vk_group.user_id != current_user.id
+    @vk_group = current_user.vk_group
   end
 
   private def vk_group_params
     p = params.require(:vk_group).permit!.to_h
-    p['albums_attributes'].delete_if { |_key, value| value['album_id'].empty? }
+    p['albums_attributes'].delete_if { |_key, value| value['identifier'].empty? }
     p
   end
 end
--- spec/controllers/vk_groups_controller_spec.rb
+++ spec/controllers/vk_groups_controller_spec.rb
@@ -1,2 +1,28 @@
 RSpec.describe VkGroupsController, type: :controller do
+  describe 'PATCH #update' do
+    it_behaves_like 'Check access'
+
+    context 'for logged user' do
+      sign_in_user
+      let(:request) { patch :update, params: { id: @current_user.vk_group.id, vk_group: { identifier: '000', albums_attributes: { '0' => { 'name' => 'Базовая коллекция', 'identifier' => '116318831' } } } } }
+
+      it 'updates sites settings' do
+        old_site = @current_user.vk_group
+        request
+        @current_user.vk_group.reload
+
+        expect(@current_user.vk_group).to eq old_site
+      end
+
+      it 'redirect_to integration page' do
+        request
+
+        expect(response).to redirect_to integration_path(@current_user)
+      end
+    end
+
+    def do_request
+      patch :update, params: { id: 999, product: {} }
+    end
+  end
 end
