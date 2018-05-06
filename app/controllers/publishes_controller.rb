4f3b2de9b4c9f03bb494d8191a24250af4c4d408
kortirso <kortirso@gmail.com>
2018-05-06 12:56:54 +0700

    comment publishes controller

--- app/controllers/publishes_controller.rb
+++ app/controllers/publishes_controller.rb
@@ -1,21 +1,21 @@
 class PublishesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: %i[create update destroy]
-  before_action :find_product, only: %i[create destroy]
-  before_action :find_publish, only: %i[show update destroy]
-  before_action :check_publish, only: %i[show]
+  # before_action :find_product, only: %i[create destroy]
+  # before_action :find_publish, only: %i[show update destroy]
+  # before_action :check_publish, only: %i[show]
 
   def show
-    @albums = current_user.albums.list
+    # @albums = current_user.albums.list
   end
 
   def create
-    publish = Publish.new(product: @product, user: current_user)
-    if publish.save
+    # publish = Publish.new(product: @product, user: current_user)
+    # if publish.save
       # redirect_to product_publish_path(@product, publish)
-      redirect_to @product
-    else
-      redirect_to @product
-    end
+    #  redirect_to @product
+    # else
+    #  redirect_to @product
+    # end
   end
 
   def update
@@ -33,21 +33,21 @@ class PublishesController < ApplicationController
     # redirect_to @product
   end
 
-  private def find_product
-    @product = Product.friendly.find(params[:product_id])
-    render_not_found if @product.nil?
-  end
+  # private def find_product
+    # @product = Product.friendly.find(params[:product_id])
+    # render_not_found if @product.nil?
+  # end
 
-  private def find_publish
-    @publish = Publish.find_by(id: params[:id])
-    render_not_found if @publish.nil?
-  end
+  # private def find_publish
+    # @publish = Publish.find_by(id: params[:id])
+    # render_not_found if @publish.nil?
+  # end
 
-  private def check_publish
-    render_not_found if current_user.id != @publish.user_id
-  end
+  # private def check_publish
+    #render_not_found if current_user.id != @publish.user_id
+  # end
 
-  private def publish_params
-    params.require(:publish).permit(:name, :caption, :price, :album_id)
-  end
+  # private def publish_params
+    # params.require(:publish).permit(:name, :caption, :price, :album_id)
+  # end
 end
